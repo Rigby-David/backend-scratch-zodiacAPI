46b536f6e9ebad6895552bb3d4da7d9756c9f7b2
David <Rigby335@hotmail.com>
2022-07-28 15:31:33 -0700

    tested both routes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,8 +7,24 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('GET /discs should return a list of discs', async () => {
+    const resp = await request(app).get('/discs');
+    expect(resp.body.length).toEqual(6);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+    });
+  });
+  it('GET /discs/:id should return a disc with details by id', async () => {
+    const resp = await request(app).get('/discs/1');
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      type: expect.any(String),
+      speed: expect.any(Number),
+      glide: expect.any(Number),
+    });
   });
   afterAll(() => {
     pool.end();
