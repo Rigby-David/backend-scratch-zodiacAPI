a05a87576a72115eb1e1f6941ed5e282c0481b87
David <Rigby335@hotmail.com>
2022-07-28 14:46:42 -0700

    seeded data and confirmed w/ beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,20 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists discs;
+
+CREATE table discs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    type VARCHAR NOT NULL,
+    speed INT,
+    glide INT
+);
+
+INSERT INTO discs (name, type, speed, glide) VALUES
+('Wraith', 'Driver', 11, 5),
+('Whale', 'Putter', 2, 3),
+('Buzzz', 'Midrange', 5, 4),
+('Nuke', 'Driver', 13, 5),
+('Explorer', 'Driver', 7, 5),
+('Raptor', 'Driver', 9, 4);
